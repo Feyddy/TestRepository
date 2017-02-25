c557e1c93f1ddbe75a59e31d8280954da39ea031
Feyddy <Feyddyxu@163.com>
2017-02-25 11:22:03 +0800

    提交第二次版本

--- TestRepositoryDemo/TestRepositoryDemo/ViewController.swift
+++ TestRepositoryDemo/TestRepositoryDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("我是版本一")
+        print("我是版本二")
     }
 
     override func didReceiveMemoryWarning() {
