d47e787ad8bf3f8e122f0fa4e0ca153fd26b481b
Feyddy <Feyddyxu@163.com>
2017-02-25 11:13:24 +0800

    第一次提交版本一

---
+++ TestRepositoryDemo/TestRepositoryDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestRepositoryDemo
+//
+//  Created by t3 on 2017/2/25.
+//  Copyright © 2017年 feyddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("我是版本一")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
